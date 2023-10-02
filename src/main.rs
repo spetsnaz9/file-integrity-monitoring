55f08c149287f6eb23f4b8aeebb421eedd6bea2b
Valentin <valentin.cudey@epita.fr>
2023-10-02 10:59:53 +0200

    add recursive inotify

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate inotify;
 
 use inotify::{Inotify, WatchMask, EventMask};
 use std::env;
-
-
+use std::collections::HashMap;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn check_path(desired_path: &str) -> Result<(), ()> {
     if let Ok(current_path) = env::current_dir() {
@@ -18,6 +19,33 @@ fn check_path(desired_path: &str) -> Result<(), ()> {
     Err(())
 }
 
+fn watch_directory_recursive(
+    inotify: &Inotify,
+    dir: &Path,
+    watched_dirs: &mut HashMap<i32, PathBuf>,
+) -> Result<(), Box<dyn std::error::Error>> {
+
+    let dir_metadata = fs::metadata(dir)?;
+
+    if dir_metadata.is_dir() {
+        let wd = inotify.watches().add(
+            dir,
+            WatchMask::MODIFY | WatchMask::DELETE | WatchMask::CREATE,
+        )?;
+
+        let wd_id: i32 = wd.get_watch_descriptor_id();
+        watched_dirs.insert(wd_id, dir.to_path_buf());
+        let dir_entries = fs::read_dir(dir)?;
+
+        for entry in dir_entries {
+            if let Ok(entry) = entry {
+                watch_directory_recursive(inotify, &entry.path(), watched_dirs)?;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
     let desired_path = "/home/spetsnaz/projets/fms/test";
@@ -25,31 +53,25 @@ fn main() {
         Err(_) => {
             println!("Bad path");
             return;
-        },
-        _ => ()
+        }
+        _ => (),
     }
 
-    let mut inotify = Inotify::init()
-        .expect("Failed to initialize inotify");
+    let mut inotify = Inotify::init().expect("Failed to initialize inotify");
+    let mut watched_dirs: HashMap<i32, PathBuf> = HashMap::new();
 
-    inotify
-        .watches()
-        .add(
-            desired_path, 
-            WatchMask::MODIFY | WatchMask::DELETE | WatchMask::CREATE)
-        .expect("Failed to add notify watch");
+    watch_directory_recursive(&inotify, Path::new(&desired_path), &mut watched_dirs)
+        .expect("Failed to watch directories");
 
     let mut buffer = [0; 4096];
     loop {
-        let events = inotify
-            .read_events_blocking(&mut buffer)
-            .expect("Error while reading events");
+        let events = inotify.read_events_blocking(&mut buffer).expect("Error while reading events");
 
         for event in events {
             if !event.mask.contains(EventMask::ISDIR) {
                 let name_file = match event.name {
                     Some(name_file) => name_file,
-                    None => continue
+                    None => continue,
                 };
 
                 match event.mask {
